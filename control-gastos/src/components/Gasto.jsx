a0d84bf6630217ff921e389e97a80f46fa7cbe01
AlexeiNay <alexeisanchez66@gmail.com>
2022-11-13 16:14:30 -0700

    Swipe animation need

--- control-gastos/src/components/Gasto.jsx
+++ control-gastos/src/components/Gasto.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import {
+    LeadingActions,
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions
+} from 'react-swipeable-list'
+import 'react-swipeable-list/dist/styles.css'
 import { formatearFecha } from '../helpers'
 import IconoAhorro from '../img/icono_ahorro.svg'
 import IconoCasa from '../img/icono_casa.svg'
@@ -21,6 +29,8 @@ const diccionarioIconos = {
 const Gasto = ({ gasto }) => {
     const { categoria, nombre, cantidad, id, fecha } = gasto
     return (
+        <SwipeableList>
+            <SwipeableListItem>
         <div className='gasto sombra'>
             <div className='contenido-gasto'>
                 <img 
@@ -35,6 +45,8 @@ const Gasto = ({ gasto }) => {
             </div>
             <p className='cantidad-gasto'>${cantidad}</p>
         </div>
+        </SwipeableListItem>
+        </SwipeableList>
     )
 }
 
