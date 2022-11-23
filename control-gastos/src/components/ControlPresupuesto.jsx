b9ee5f7329d9ce4a404471cb8f4082e50201ff08
AlexeiNay <alexeisanchez66@gmail.com>
2022-11-22 19:24:31 -0600

    Final changes

--- control-gastos/src/components/ControlPresupuesto.jsx
+++ control-gastos/src/components/ControlPresupuesto.jsx
@@ -1,42 +1,77 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+import "react-circular-progressbar/dist/styles.css"
 
-const ControlPresupuesto = ({presupuesto, gastos}) => {
+const ControlPresupuesto = ({
+    presupuesto,
+    setPresupuesto,
+    gastos,
+    setGastos,
+    setIsValidPresupuesto
+}) => {
 
+    const [porcentaje, setPorcentaje] = useState(0)
     const [disponible, setDisponble] = useState(0)
     const [gastado, setGastado] = useState(0)
 
-    useEffect(()=>{
+    useEffect(() => {
         const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0)
         const totalDisponible = presupuesto - totalGastado
 
+        //Calcular el porcentaje gastado
+        const nuevoPorcentaje = (((presupuesto - totalDisponible) / presupuesto) * 100).toFixed(2)
+
         setGastado(totalGastado)
         setDisponble(totalDisponible)
+        setTimeout(() => {
+            setPorcentaje(nuevoPorcentaje)
+        }, 1500);
     }, [gastos])
 
-    const formatearCantidad = (cantidad)=>{
-        return cantidad.toLocaleString('en-US',{
-            style:'currency',
-            currency:'USD'
+    const formatearCantidad = (cantidad) => {
+        return cantidad.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
         })
     }
-  return (
-    <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
-        <div>
-            <p>Grafica aquí</p>
+
+    const handleResetApp = () => {
+        const resultado = confirm('¿Deseas reiniciar presupuesto y gastos?')
+            if(resultado){
+                setGastos([])
+                setPresupuesto(0)
+                setIsValidPresupuesto(false)
+            }
+    }
+    return (
+        <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
+            <div>
+                <CircularProgressbar
+                    styles={buildStyles({
+                        pathColor: porcentaje > 100 ? '#DC2626' : '#000000',
+                        trailColor: '#7E7E7E',
+                        textColor: porcentaje > 100 ? '#DC2626' : '#000000',
+                    })}
+                    value={porcentaje}
+                    text={`${porcentaje}% Gastado`}
+                />
+            </div>
+            <div className='contenido-presupuesto'>
+                <button className='reset-app' type='button' onClick={handleResetApp}>
+                    Resetear app
+                </button>
+                <p>
+                    <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
+                </p>
+                <p className={`${disponible < 0 ? 'negativo' : ''}`}>
+                    <span>Disponible: </span> {formatearCantidad(disponible)}
+                </p>
+                <p>
+                    <span>Gastado: </span> {formatearCantidad(gastado)}
+                </p>
+            </div>
         </div>
-        <div className='contenido-presupuesto'>
-            <p>
-                <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
-            </p>
-            <p>
-                <span>Disponible: </span> {formatearCantidad(disponible)}
-            </p>
-            <p>
-                <span>Gastado: </span> {formatearCantidad(gastado)}
-            </p>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default ControlPresupuesto
--- control-gastos/src/components/Gasto.jsx
+++ control-gastos/src/components/Gasto.jsx
@@ -17,35 +17,56 @@ import IconoSalud from '../img/icono_salud.svg'
 import IconoSuscipciones from '../img/icono_suscripciones.svg'
 
 const diccionarioIconos = {
-        ahorro: IconoAhorro,
-        comida: IconoComida,
-        casa: IconoCasa,
-        gastos: IconoGastos,
-        ocio: IconoOcio,
-        salud: IconoSalud,
-        suscripciones: IconoSuscipciones
+    ahorro: IconoAhorro,
+    comida: IconoComida,
+    casa: IconoCasa,
+    gastos: IconoGastos,
+    ocio: IconoOcio,
+    salud: IconoSalud,
+    suscripciones: IconoSuscipciones
 }
 
-const Gasto = ({ gasto }) => {
+const Gasto = ({ gasto, setGastoEditar, eliminarGasto }) => {
     const { categoria, nombre, cantidad, id, fecha } = gasto
+
+    const leadingActions = () => (
+        <LeadingActions>
+            <SwipeAction onClick={() => setGastoEditar(gasto)}>
+                Editar
+            </SwipeAction>
+        </LeadingActions>
+    )
+    const trailingActions = () => (
+        <TrailingActions>
+            <SwipeAction 
+                onClick={() => eliminarGasto(id)}
+                destructive={true}
+                >
+                Borrar
+            </SwipeAction>
+        </TrailingActions>
+    )
     return (
         <SwipeableList>
-            <SwipeableListItem>
-        <div className='gasto sombra'>
-            <div className='contenido-gasto'>
-                <img 
-                    src={diccionarioIconos[categoria]}
-                />
-                <div className='descripcion-gasto'>
-                    <p className='categoria'>{categoria}</p>
-                    <p className='nombre-gasto'>{nombre}</p>
-                    <p className='fecha-gasto'>Agregado el: {''}<span>{formatearFecha(fecha)}</span></p>
-                </div>
+            <SwipeableListItem
+                leadingActions={leadingActions()}
+                trailingActions={trailingActions()}
+            >
+                <div className='gasto sombra'>
+                    <div className='contenido-gasto'>
+                        <img
+                            src={diccionarioIconos[categoria]}
+                        />
+                        <div className='descripcion-gasto'>
+                            <p className='categoria'>{categoria}</p>
+                            <p className='nombre-gasto'>{nombre}</p>
+                            <p className='fecha-gasto'>Agregado el: {''}<span>{formatearFecha(fecha)}</span></p>
+                        </div>
 
-            </div>
-            <p className='cantidad-gasto'>${cantidad}</p>
-        </div>
-        </SwipeableListItem>
+                    </div>
+                    <p className='cantidad-gasto'>${cantidad}</p>
+                </div>
+            </SwipeableListItem>
         </SwipeableList>
     )
 }
